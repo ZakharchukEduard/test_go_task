15f018f03ab991e31c6b8e9eef55dce7597726de
ZakharchukEduard <zakharchuk.ed@gmail.com>
2021-01-06 09:24:45 +0200

    Interface

---
+++ handler/auth.go
@@ -0,0 +1,13 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+// Todo ...
+func (h *Handler) SingUp(c *gin.Context) {
+
+}
+
+// Todo ...
+func (h *Handler) SingIn(c *gin.Context) {
+
+}
--- handler/handler.go
+++ handler/handler.go
@@ -9,26 +9,26 @@ func (h *Handler) InitRoutes() *gin.Engine {
 	router := gin.New()
 	auth := router.Group("/auth")
 	{
-		auth.POST("/sing-up")
-		auth.POST("/sing-in")
+		auth.POST("/sing-up", h.SingUp)
+		auth.POST("/sing-in", h.SingIn)
 	}
 	api := router.Group("/api")
 	{
 		lists := api.Group("/lists")
 		{
-			lists.POST("/")
-			lists.GET("/")
-			lists.GET("/:id")
-			lists.PUT("/:id")
-			lists.DELETE("/:id")
+			lists.POST("/", h.createList)
+			lists.GET("/", h.getAllLists)
+			lists.GET("/:id", h.getListByID)
+			lists.PUT("/:id", h.updateList)
+			lists.DELETE("/:id", h.deleteList)
 
 			items := lists.Group(":id/items")
 			{
-				items.POST("/")
-				items.GET("/")
-				items.GET("/:item_id")
-				items.PUT("/:item_id")
-				items.DELETE("/:item_id")
+				items.POST("/", h.createItem)
+				items.GET("/", h.getAllItems)
+				items.GET("/:item_id", h.getItemByID)
+				items.PUT("/:item_id", h.updateItem)
+				items.DELETE("/:item_id", h.deleteItem)
 			}
 		}
 	}
